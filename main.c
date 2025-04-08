2ed83020d7dc122b40ec9ed53126c7ec246fd879
ivanvillanueva-cloud <ivanvillauni@gmail.com>
2025-04-08 15:13:23 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,21 +1,34 @@
-/*GPIO functional description (p.158)
-Memory and bus architecture (p.46)
-
-*/
-
 #include <stm32f10x.h>
 #include "garrita.h"
 #include "interrupts.h"
 #include "gpio.h"
+#include "delay.h"
+
+
+
 
 int main(void)
 {
-  init_gpio();
-  init_interruputs();
-	timer2_pwm();
-	timer3_pwm();
-	while(1)
-	{
-		
- }
-	}
+    init_gpio();
+    init_interruputs();
+    timer2_pwm();
+    timer3_pwm();
+	  void delay_ms(uint32_t ms);
+    
+    while(1)
+    {
+        
+            motor2_secuencia1(); // 0°->90°->180°
+			delay_ms(1000);
+            motor1_secuencia2();
+			delay_ms(100000);
+		}
+        
+            motor2_secuencia1(); // 0°->90°->180°
+            delay_ms(1000);
+            motor2_secuencia2();
+			delay_ms(10000);
+        }
+
+
+
